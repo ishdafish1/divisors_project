8fd38c493d4124aa229355fd2d0f4c8eb45b3284
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-12-27 20:45:13 +0000

    Add iteration capability, and add customisation options

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 def div(num):
-  divisors = []
-  sum = 0
-  for i in range(1, num+1):
-    if num % i == 0:
-      divisors.append(i)
-      sum += i
-  print('The divisors of your numbers are: ' + str(divisors))
-  print('Sum of its divisors: ' + str(sum))
-  
+    divisors = []
+    sum = 0
+    for i in range(1, int(num/2)+1): # there will be no divisors beyond half the value of num
+        if num % i == 0:
+            divisors.append(i)
+            sum += i
+# print('The divisors of your numbers are: ' + str(divisors))
+#  print('Sum of its divisors: ' + str(sum))
+    return sum
+
+startPoint = 0 # number to begin testing from
+maxAttempts = 100 # maximum number of attempts before program stops
+divisorSum = 2018 # the divisor sum you are searching for
+success = False # shows whether a number with a satisfactory divisor sum has been found
+
+attempts = 0
+
+while attempts < maxAttempts:
+    if div(startPoint + attempts) == divisorSum:
+        success = True
+        break
+    else:
+        attempts += 1
+
+if success:
+    print('The number ' + str(startPoint + attempts) + ' has a divisor sum of ' + str(divisorSum) + '.')
+else:
+    print('Program exceeded maximum number of attempts, without success.')
